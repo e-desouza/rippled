2828fb635bc89ec06f0e3446d55a8b2fec61a252
e-desouza <edesouza@ripple.com>
2026-01-23 00:05:18 +0000

    refactor(levelization): break InfoSub.h dependency on Manifest.h
    Replace #include <xrpld/app/misc/Manifest.h> with forward declaration
    of Manifest struct. This breaks a key header-level dependency that
    was contributing to the app <-> rpc cycle.
    
    The Manifest type is only used as a const reference parameter in
    pubManifest(), which doesn't require the full definition.

--- src/xrpld/rpc/InfoSub.h
+++ src/xrpld/rpc/InfoSub.h
@@ -1,8 +1,6 @@
 #ifndef XRPL_NET_INFOSUB_H_INCLUDED
 #define XRPL_NET_INFOSUB_H_INCLUDED
 
-#include <xrpld/app/misc/Manifest.h>
-
 #include <xrpl/basics/CountedObject.h>
 #include <xrpl/json/json_value.h>
 #include <xrpl/protocol/Book.h>
@@ -10,6 +8,8 @@
 #include <xrpl/resource/Consumer.h>
 
 namespace xrpl {
+
+struct Manifest;
 
 // Operations that clients may wish to perform against the network
 // Master operational handler, server sequencer, network tracker
